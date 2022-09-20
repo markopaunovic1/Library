98f4dce7bac8ebec3187ce81e30ddfc0ced8d1e0
markopaunovic1 <marpau970514@hotmail.com>
2022-09-20 15:41:35 +0200

    clearing codes

--- src/Book.java
+++ src/Book.java
@@ -3,17 +3,20 @@ public class Book {
 
     String bookName;
     String writer;
+
     int bookYear;
+
     String edition;
     boolean available;
 
     String loan;
 
-    public Book (String bN, String writer) {
+    public Book (String bN, String writer, int bY, String edition) {
         this.bookName = bN;
         this.writer = writer;
         this.available = true;
-
+        this.bookYear = bY;
+        this.edition = edition;
     }
 
     boolean isAvailable () {
@@ -23,7 +26,7 @@ public class Book {
 
     @Override
     public String toString(){
-        return "TITLE: " + bookName + ", AUTHOR: " + writer + ", LOANED: " + !available;
+        return "TITLE: " + bookName + ", AUTHOR: " + writer + ", Year: " + bookYear + ", Edition: " + edition + ", LOANED: " + !available;
     }
 
 
--- src/Library.java
+++ src/Library.java
@@ -6,42 +6,43 @@ public class Library {
     static ArrayList<Book> listOfBooks = new ArrayList<>();
     Scanner scan = new Scanner(System.in);
 
-    public static void addInitialBooks(){
-        addBookToLibrary(new Book("The Lord of the Rings", "RR Tolkien"));
-        addBookToLibrary(new Book("A Game of Thrones", "George R.R Martin"));
-        addBookToLibrary(new Book("Java for dummies", "B Burd"));
-        addBookToLibrary(new Book("Java for experts", "B Burd"));
-        addBookToLibrary(new Book("Harry Potter", "JK.Rowling"));
+    public static void addInitialBooks() {
+        addBookToLibrary(new Book("The Lord of the Rings", "RR Tolkien",1788, "1"));
+        addBookToLibrary(new Book("A Game of Thrones", "George R.R Martin",1928, "2"));
+        addBookToLibrary(new Book("Java for dummies", "B Burd",1838, "3"));
+        addBookToLibrary(new Book("Java for experts", "B Burd",8383,"4"));
+        addBookToLibrary(new Book("Harry Potter", "JK.Rowling",2002, "5"));
 
     }
 
-    public static void addBookToLibrary(Book newBook){
+    public static void addBookToLibrary(Book newBook) {
         //denna funktionen lägger till en ny book
         listOfBooks.add(newBook);
     }
 
     public void searchBook(String searchInput) {
 
-
-        for (Book b: listOfBooks) {
-            if (b.bookName.equals(searchInput)){
+        for (Book b : listOfBooks) {
+            if (b.bookName.equalsIgnoreCase(searchInput)) {
                 System.out.println("Found book: " + b);
 
-            if(b.isAvailable() == true){
+                if (b.isAvailable() == true) {
 
-            System.out.println("Do you want to borrow it? yes/no?");
-            String wantsToBorrow = scan.nextLine();
+                    System.out.println("Do you want to borrow it? yes/no?");
+                    String wantsToBorrow = scan.nextLine();
 
-            if (wantsToBorrow.equals("yes")) {
-                loan(b);
-                break;
-            }}}}
+                    if (wantsToBorrow.equalsIgnoreCase("yes")) {
+                        loan(b);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
+    public static void loan(Book book) {
 
-    public static void loan(Book book){
-
-        if(book.isAvailable() == true){
+        if (book.isAvailable() == true) {
             book.available = false;
             System.out.println("You have now borrowed the book");
         } else {
@@ -49,23 +50,37 @@ public class Library {
         }
     }
 
-    public void printListOfBooks(){
+
+    public void printListOfBooks() {
+
         //Loopar igenom listan av böcker (listOfBooks)
-        for (Book book: listOfBooks) {
+
+        for (Book book : listOfBooks) {
+
             //här printar vi ut varje Book.toString()
             // .toString är overridat i Book.java så vi kan printa ut det vi vill se
-            //if(book.isAvailable() == true) {
-                System.out.println(book.toString());
-                System.out.println();
-            //}
+
+            if(book.isAvailable() == true) {
+            System.out.println(book);
+            System.out.println();
+            }
         }
     }
 
-    public void returnBook() {
 
+    public void returnBook(String bookToReturn) {
 
+        for (Book b : listOfBooks) {
+            if (b.bookName.equalsIgnoreCase(bookToReturn)) {
 
-
+                if (b.isAvailable() == false) {
+                    b.available = true;
+                    System.out.println("You have now returned the book.");
+                } else {
+                    System.out.println("Could not find the book.");
+                }
+            }
+        }
     }
 }
 
